3f739f568f20f01f33052935e82d7f328976a3a0
AhshanHabib26 <habib.net27@gmail.com>
2022-05-22 07:23:16 +0600

    Mongo Server Setup

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_ADMIN}:${process.env.USER_PASS}@cluster0.b30yd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 
-app.get('/', (req, res) => {
-    res.send('Welcome! Motor Parts Manufacturing Website')
-})
+async function motoRun(){
+    try{
+
+        await client.connect()
+
+        const productCollection = client.db('MotoCollection').collection('products')
+
+        app.get('/product', async (req, res) =>{
+            const query = {}
+            const data = productCollection.find(query)
+            const result = await data.toArray()
+            res.send(result)
+        })
+
+        console.log('Connect')
+
+    }
+    finally{
+
+    }
+}
+motoRun()
 
 
+app.get("/", (req, res) => {
+  res.send("Welcome! Motor Parts Manufacturing Website");
+});
 
-
-
-app.listen(port)
+app.listen(port);
