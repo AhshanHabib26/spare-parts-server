4e5a0fe7ee0fd3223a7004c444117e2a13343c82
AhshanHabib26 <habib.net27@gmail.com>
2022-05-23 23:19:34 +0600

    JWT Token API Create

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middlle Ware
 app.use(cors());
@@ -19,52 +18,122 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
-async function motoRun(){
-    try{
-
-        await client.connect()
-
-        const productCollection = client.db('MotoCollection').collection('products')
-        const userCollection = client.db('MotoCollection').collection('user')
-        const productsCollection = client.db('MotoCollection').collection('userproducts')
-
-        app.get("/product", async (req, res) =>{
-            const query = {}
-            const data = productCollection.find(query)
-            const result = await data.toArray()
-            res.send(result)
-        })
-
-
-        app.get("/product/:id", async (req, res) => {
-          const id = req.params.id;
-          const query = { _id: ObjectId(id) };
-          const result = await productCollection.findOne(query)
-          res.send(result);
-        });
-
-        app.post('/user' , async(req, res) =>{
-          const data = req.body
-          const query = await userCollection.insertOne(data)
-          res.send(query)
-        })
-
-        app.post('/userproducts' , async(req, res) =>{
-          const data = req.body
-          const query = await productsCollection.insertOne(data)
-          res.send(query)
-        })
-
-        
-
+// Verify JSON WEB TOKEN Function
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
     }
-    finally{
-
-    }
+    req.decoded = decoded;
+    next();
+  });
 }
-motoRun().catch(console.dir())
 
+async function motoRun() {
+  try {
+    await client.connect();
+
+    const productCollection = client
+      .db("MotoCollection")
+      .collection("products");
+    const userCollection = client.db("MotoCollection").collection("user");
+    const productsCollection = client
+      .db("MotoCollection")
+      .collection("userproducts");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const data = productCollection.find(query);
+      const result = await data.toArray();
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { Email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ Email: email }, process.env.JWT_ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ result, token });
+    });
+
+    app.get("/user", verifyJWT, async (req, res) => {
+      const query = {};
+      const data = userCollection.find(query);
+      const result = await data.toArray();
+      res.send(result);
+    });
+
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({Email: email})
+      const userAdmin = user.role === 'admin'
+      res.send({admin: userAdmin})
+    })
+
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodeEmail = req.decoded.Email;
+      const decodeResult = await userCollection.findOne({ Email: decodeEmail });
+      if (decodeResult.role === "admin") {
+        const filter = { Email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+       return res.send(result);
+      } else {
+        return res.status(403).send({ message: "Forbiden Access" });
+      }
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ Email: email });
+      const userAdmin = user.role === "admin";
+      res.send({ admin: userAdmin });
+    });
+
+    app.post("/userproducts", async (req, res) => {
+      const data = req.body;
+      const query = await productsCollection.insertOne(data);
+      res.send(query);
+    });
+
+    app.get("/userproducts", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodeEmail = req.decoded.Email;
+      if (email === decodeEmail) {
+        const query = { Email: email };
+        const additem = productsCollection.find(query);
+        const result = await additem.toArray();
+        return res.send(result);
+      } else {
+        return res.status(403).send({ message: "Forbiden Access" });
+      }
+    });
+  } finally {
+  }
+}
+motoRun().catch(console.dir());
 
 app.get("/", (req, res) => {
   res.send("Welcome! Motor Parts Manufacturing Website");
