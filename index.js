27623543df762eb7ade4dabbeece49e3f4771c01
AhshanHabib26 <habib.net27@gmail.com>
2022-05-24 16:22:58 +0600

    Delete API Create

--- index.js
+++ index.js
@@ -110,6 +110,13 @@ async function motoRun() {
       res.send(result);
     });
 
+     app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get('/admin/:email', async(req, res) =>{
       const email = req.params.email;
       const user = await userCollection.findOne({Email: email})
