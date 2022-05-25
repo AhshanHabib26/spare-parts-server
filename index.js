9b634cc6957b3d46c7af91b10a93d2479dd6a9d8
AhshanHabib26 <habib.net27@gmail.com>
2022-05-25 16:47:44 +0600

    Order Collection API Create

--- index.js
+++ index.js
@@ -200,6 +200,11 @@ async function motoRun() {
       res.send(query);
     });
 
+    app.get( "/userproducts", async (req, res) =>{
+      const data = await productsCollection.find().toArray()
+      res.send(data)
+    })
+
     app.get("/userproducts/:id", async(req, res) =>{
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
